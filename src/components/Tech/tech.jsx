e79345b76f51de6dff3805ecb03ae69410d3cbef
Rudraksha Singh Sengar <rudraksharss@gmail.com>
2024-11-18 14:44:09 +0530

    folder management

---
+++ src/components/Tech/tech.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Tech(){
+    return(
+        <div className="tech">
+        <section className="heading">
+        <span className="heading_text">Technologies</span>
+        <span className="subheading">Things I know</span>
+        </section>
+        </div>
+    );
+}
+
+export default Tech;
